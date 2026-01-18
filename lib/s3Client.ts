147037f196d15f80a8a813281ce62f532c613cd5
Maron <vinci.ta9t0tle@gmail.com>
2026-01-19 02:22:04 +0900

    fix: s3Client for Cloudflare Edge

--- lib/s3Client.ts
+++ lib/s3Client.ts
@@ -16,7 +16,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const r2 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: process.env.R2_ENDPOINT, // ここで R2_ENDPOINT を直接使う
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
